702dee6d056d37c122b1792e44a8779bb85fb4bd
Sagar Purswani <purswani.sagar@protonshub.com>
2020-03-19 17:00:25 +0530

    Added a rails admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_01_104323) do
+ActiveRecord::Schema.define(version: 2020_03_19_105430) do
 
   create_table "channels", force: :cascade do |t|
     t.string "name"
